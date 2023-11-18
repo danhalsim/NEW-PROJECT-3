6a49964bdbd231a6e4f46a016eaf64073e29a006
dansim6935 <dansim6935@gmail.com>
2023-11-17 19:50:12 -0800

    Added header

---
+++ client/src/components/Header.js
@@ -0,0 +1,26 @@
+function Header() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <strong>Todo List</strong>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <a href="#">Link</a>
+        </li>
+        <li>
+          <a href="#">Link</a>
+        </li>
+        <li>
+          <a href="#" role="button">
+            Button
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Header;
